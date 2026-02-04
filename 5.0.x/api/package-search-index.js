f4d50b1b801898206fba08f277f37bd8860b35a9
lissam1 <lissam1@users.noreply.github.com>
2026-02-04 17:01:38 +0000

    Deploying to gh-pages - 17:01:38

---
+++ 5.0.x/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.context.env.groovy"},{"l":"io.micronaut.function.groovy"},{"l":"io.micronaut.groovy.info"}];updateSearchResults();
